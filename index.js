40ab2c51cf1fd4170042df3fa9e732a750843580
Valeria Copleman <valeriacopleman@gmail.com>
2021-02-18 20:15:08 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove() 
+
+let newHeader = document.createElement('h1')
+
+
+//let newHeader = document.querySelector('h1#victory');
+
+//let newHeader = document.getElementById("h1#victory");
+
+//let element = document.querySelector("p#greeting");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "VALERIA is the champion";
+
